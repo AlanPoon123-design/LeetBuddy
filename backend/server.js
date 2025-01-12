d5afa28797f9d13f7753b489437f5e65e9c7edb5
Lucian Cheng <luciancheng3@gmail.com>
2025-01-12 10:24:47 -0500

    changed port

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const { initializeRedisClient } = require("./services/LLM_config.js");
 // APP INITIALIZATION
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ limit: "4mb", extended: true }));
 
